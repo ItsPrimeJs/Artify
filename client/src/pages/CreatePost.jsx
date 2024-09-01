2659fb0dc9c0bc20e144edc9353ef531d4449d6f
ItsPrimeJs <sujalshelar918@gmail.com>
2024-09-01 16:33:47 +0530

    Complete front end Done

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -11,6 +11,7 @@ const CreatePost = () => {
     prompt: "",
     photo: "",
   });
+  
   const [generatingImage, setGeneratingImage] = useState(false);
 
   const [Loading, setLoading] = useState(false);
